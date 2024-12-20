4a4b55323bda9af13d324af929ec10092c054c1e
Santi-Lp <santiagolopriore@gmail.com>
2024-12-19 22:17:57 -0300

    Version Final Frontend

--- src/fetching/htp.fetching.js
+++ src/fetching/htp.fetching.js
@@ -2,7 +2,6 @@ export const POST = async (URL_API, params) => {
     try {
         const response = await fetch(URL_API, {
             method: 'POST',
-            credentials: "include",
             ...params
         })
         return response.json();
@@ -25,7 +24,7 @@ const getAuthenticatedHeaders = () => {
 const authenticatedHeaders = new Headers();
 authenticatedHeaders.set('Content-Type', 'application/json');
 authenticatedHeaders.set("x-api-key","cacda5fe-c409-441a-bbbf-1788e204ddc9");
-authenticatedHeaders.set("Authorization", `Bearer ${sessionStorage.getItem("access_token")}`);
+authenticatedHeaders.set("Authorization", ` ${sessionStorage.getItem("access_token")}`);
 
 return authenticatedHeaders
 }
